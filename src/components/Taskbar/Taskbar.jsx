dd12c635e17adfd0770cb3023bfef01060221703
BennDoverr <92238696+BennDoverr@users.noreply.github.com>
2025-01-25 00:13:32 -0700

    p, added options for taskbar

--- src/components/Taskbar/Taskbar.jsx
+++ src/components/Taskbar/Taskbar.jsx
@@ -1,11 +1,30 @@
 import React from "react";
+import styles from './Taskbar.module.css';
 
 export default function Taskbar () {
-    const [users, setUsers] = React.useState([]);
     
     return (
-        <div>
+        <div className={styles.container}>
+            <div className={styles.logo}> 
+                <img src="" alt="logo" />
+            </div>
+            <ul className={styles.menu}>
+                <li><img src="" alt="home" /> Home</li>
+                <li><img src="" alt="search" /> Search</li>
+                <li><img src="" alt="explore" /> Explore</li>
+                <li><img src="" alt="reels" /> Reels</li>
+                <li><img src="" alt="messages" /> Messages</li>
+                <li><img src="" alt="explore" /> Explore</li>
+                <li><img src="" alt="notifications" /> Notifications</li>
+                <li><img src="" alt="create" /> Create</li>
+                <li><img src="" alt="profile" /> Profile</li>
+            </ul>
 
+            <ul>
+                <li><img src="" alt="AI Studio" /> AI Studio</li>
+                <li><img src="" alt="Threads" /> Threads</li>
+                <li><img src="" alt="More" /> More</li>
+            </ul>
         </div>
     );
-};
+}
